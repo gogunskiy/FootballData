476a80574f9d7ea79d6cdc6ede1bb049432f7b7f
Volodymyr Hohunskyi <volodymyr.hohunskyi@accenture.com>
2016-11-07 15:50:13 +0200

    Added initial pod structure

---
+++ Example/Pods/Target Support Files/Pods-FootballData_Example/Pods-FootballData_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FootballData_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FootballData_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-FootballData_Tests/Pods-FootballData_Tests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_FootballData_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FootballData_TestsVersionString[];
+
