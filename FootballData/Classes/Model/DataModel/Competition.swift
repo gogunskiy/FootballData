45ee7c084dd0deaa90dec4934d5992a11f0fa132
Volodymyr Hohunskyi <volodymyr.hohunskyi@accenture.com>
2016-11-07 17:02:23 +0200

    Added data model

---
+++ FootballData/Classes/Model/DataModel/Competition.swift
@@ -0,0 +1,13 @@
+//
+//  Competition.swift
+//  Pods
+//
+//  Created by Hohunskyi, Volodymyr on 11/7/16.
+//
+//
+
+import UIKit
+
+class Competition: NSObject {
+
+}
---
+++ FootballData/Classes/Model/DataModel/Team.swift
@@ -0,0 +1,13 @@
+//
+//  Team.swift
+//  Pods
+//
+//  Created by Hohunskyi, Volodymyr on 11/7/16.
+//
+//
+
+import UIKit
+
+class Team: NSObject {
+
+}
